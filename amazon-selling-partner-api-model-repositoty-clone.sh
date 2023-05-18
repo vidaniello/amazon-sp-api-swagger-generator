f906ba75c08de1a56eb204de45ad3c21057c2909
Vincenzo D'Aniello <vidaniello@gmail.com>
2023-05-18 10:53:16 +0200

    first commit

---
+++ amazon-selling-partner-api-model-repositoty-clone.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# echo Directory amz-git-repo, removing...
+# rm -rf amz-git-repo
+# echo Directory amz-git-repo removed.
+git clone https://github.com/amzn/selling-partner-api-models target/amz-git-repo/
+# echo Directory amz-git-repo/.git and amz-git-repo/.github, removing...
+# rm -rf target/amz-git-repo/.git
+# rm -rf target/amz-git-repo/.github
+# echo Directory amz-git-repo/.git and amz-git-repo/.github removed.
